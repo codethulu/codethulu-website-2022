ac40db679ab3b461d02fcf8d0a2559c2af2fa0dd
codethulu <brendanbell2001@hotmail.com>
2022-05-11 12:19:18 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,219 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import { Masonry } from '@mui/lab';
 import './App.css';
+import {
+  Link,
+  Element,
+} from "react-scroll";
+import ScrollAnimation from 'react-animate-on-scroll';
+
+import logo from './images/logo.svg';
+import photo from './images/profile-pic.jpg'
+import warwick from './images/warwick.jpg'
+import twitter_logo from './images/twitter_logo.svg';
+import email_logo from './images/message-logo.svg';
+import linkedin_logo from './images/linkedin-logo.svg';
+import github_logo from './images/github-logo.svg';
+
+import projectsList from './components/projectsList';
+import './components/projectCard.css'
+import "animate.css/animate.min.css";
 
 function App() {
+  const [projects, setProjects] = useState(projectsList.filter((project: { tags: string | string[]; }) => project.tags.includes("favourites")));
+  const allProjects = () => {
+    setProjects(projectsList);
+  }
+  const filterProjects = (e: string) => {
+    setProjects(projectsList.filter((project: { tags: string | string[]; }) => project.tags.includes(e)));
+  }
+  const filterProjectsTitle = (e: string) => {
+    setProjects(projectsList.filter((project: { name: string; description: string; }) => project.name.toUpperCase().includes(e.toUpperCase()) || project.description.toUpperCase().includes(e.toUpperCase())));
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <SideLinks />
+      <div className='welcome'>
+
+        <h1> CODETHULU </h1>
+        <h2>brendan bell is a software engineer and graphic designer</h2>
+
+        <Link activeClass="active" to="about" spy={true} smooth={true} offset={0} duration={750} >
+          <div className='down-button'></div>
+        </Link>
+      </div>
+      <div className='container m-top-50'>
+        <Element name="about" className="element"></Element>
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <h1>hi, my name is Brendan.</h1>
+        </ScrollAnimation>
+        <Split style='left' text="i'm a software engineer and graphic designer, and i love to work on projects that allow me to apply my creative approach to solving problems. currently i am enjoying working on projects for the web and mobile, and game development." image={photo}></Split>
+        <Split style='right' text="i am currently studying Computer Science at the University of Warwick, where i am publicity officer at the Computing Society and deputy chair of Warwick Labour. " image={warwick}></Split>
+
+      </div>
+      <div className='container m-top-50'>
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <h1>here are some things i have made:</h1>
+        </ScrollAnimation>
+
+        <ProjectShowcase allProjects={allProjects} filterProjects={filterProjects} filterProjectsTitle={filterProjectsTitle} projects={projects} />
+
+
+      </div>
+      <div className='container m-top-50 h-full'>
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <h1>get in touch!</h1>
+        </ScrollAnimation>
+        <p className='m-top-50'>interested in working with me, or even just want to talk about any of my work? please don't hesitate to get in contact!</p>
+        <div className='center'>
+          <div className='m-top-50'></div>
+          <a href='mailto: brendan@codethulu.dev'><button className='contact-button m-top-20'><img src={email_logo} className='contact-logo'></img>  Email</button></a>
+          <a href='https://github.com/codethulu'><button className='contact-button m-top-20'><img src={github_logo} className='contact-logo'></img>  Github</button></a>
+          <a href='https://twitter.com/Codethulu_'><button className='contact-button m-top-20'><img src={twitter_logo} className='contact-logo'></img>  Twitter</button></a>
+          <a href='https://www.linkedin.com/in/brendan-bell-34b282202/'><button className='contact-button m-top-20'><img src={linkedin_logo} className='contact-logo'></img>  LinkedIn</button></a>
+
+        </div>
+
+      </div>
+    </>
+  );
+}
+export default App;
+
+interface SplitProps {
+  style: string;
+  text: string;
+  image: any;
+}
+
+
+const Split = (props: SplitProps) => {
+  switch (props.style) {
+    case 'left':
+      return (
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <div className='split-box m-top-10vh'>
+            <img src={props.image} className='split-pic greyscale'></img>
+            <div className='inline m-left-20'>
+              <p>{props.text}</p>
+            </div>
+          </div>
+        </ScrollAnimation >
+      );
+    case 'right':
+      return (
+        <ScrollAnimation animateIn="animate__fadeIn" animateOnce={true}>
+          <div className='split-box right-0 m-top-100 align-right'>
+            <div className='inline'>
+              <p>{props.text}</p>
+            </div>
+            <img src={props.image} className='split-pic m-left-20 greyscale'></img>
+
+          </div>
+        </ScrollAnimation>
+      );
+  }
+  return (<></>);
+
+}
+
+interface ProjectCardProps {
+  name: string; description: string; tags: string[]; image: string; filterProjects: any;
+}
+const ProjectCard = (props: ProjectCardProps) => {
+  return (
+
+    <div className='project-card'>
+
+
+      <div className={'image image-' + props.image}></div>
+      <h1> {props.name} </h1>
+      <p> {props.description}</p>
+      <div className='center'>
+        {props.tags.map((e) => {
+          let tagStyle = 'tag'
+          if (e === "java" || e === "javascript") {
+            tagStyle += ' orange'
+          } else if (e === "haskell") {
+            tagStyle += ' purple'
+          } else if (e === "c") {
+            tagStyle += ' green'
+          } else if (e === "postgresql") {
+            tagStyle += ' cobalt'
+          } else if (e === "html" || e === "swift" || e === "favourites") {
+            tagStyle += ' fire'
+          } else if (e === "css" || e === "typescript") {
+            tagStyle += ' ocean'
+          } else if (e === "python") {
+            tagStyle += ' snake'
+          }
+          return (
+            <button className={tagStyle} onClick={() => props.filterProjects(e)}>{e}</button>
+          );
+        })}
+      </div>
+
+    </div >
+  )
+}
+
+const ProjectShowcase = (props: { allProjects: any; filterProjects: any; filterProjectsTitle: any; projects: any }) => {
+  const [searchTerm, setSearchTerm] = React.useState("");
+  const handleSearchChange = (e: { target: { value: React.SetStateAction<string>; }; }) => {
+    setSearchTerm(e.target.value)
+    props.filterProjectsTitle(searchTerm)
+  }
+  return (
+    <>
+      {/* <Element name="projects" className="element"></Element> */}
+      <div className='divider'></div>
+
+      <div className='center m-top-50'>
+        <button className="tag fire" onClick={() => props.filterProjects("favourites")}>FAVOURITES</button>
+        <button className="tag snake" onClick={() => props.filterProjects("python")}>PYTHON</button>
+        <button className="tag orange" onClick={() => props.filterProjects("java")}>JAVA</button>
+        <button className="tag purple" onClick={() => props.filterProjects("haskell")}>HASKELL</button>
+        <button className="tag" onClick={() => props.filterProjects("web-dev")}>WEB-DEV</button>
+        <button className="tag" onClick={() => props.allProjects()}>ALL</button>
+
+        {/* <input type="text" className='searchBar' placeholder='Search' value={searchTerm} onChange={handleSearchChange}></input>
+        <div className='divider'></div> */}
+      </div>
+
+      <div className='inner-container h-auto m-top-50'>
+        <Masonry
+          columns={{ xs: 1, sm: 1, md: 2, lg: 2, xl: 3 }}
+          spacing={2}
+          defaultHeight={50}
+          defaultColumns={3}
+          defaultSpacing={2}
         >
-          Learn React
-        </a>
-      </header>
+          {props.projects.map((e: { name: string; titleCard: boolean; description: string; tags: string[]; image: string; showcase: boolean; }) => {
+            return (
+              <ProjectCard name={e.name} description={e.description} tags={e.tags} image={e.image} filterProjects={props.filterProjects} />
+            );
+          })}
+        </Masonry>
+      </div>
+
+
+    </>
+  );
+}
+const SideLinks = () => {
+  return (
+    <div>
+      <div className='side-container left-side'>
+        <a href='https://github.com/codethulu'><div className='link-logo github'></div></a>
+        <a href='https://twitter.com/Codethulu_'><div className='link-logo twitter'></div></a>
+        <a href='https://www.linkedin.com/in/brendan-bell-34b282202/'><div className='link-logo linkedin'></div></a>
+
+        <div className='line'></div>
+      </div>
+      <div className='side-container right-side'>
+        <p><a href='mailto: brendan@codethulu.dev'>brendan@codethulu.dev</a></p>
+        <div className='line'></div>
+      </div>
     </div>
   );
 }
-
-export default App;
---
+++ src/components/projectsList.tsx
@@ -0,0 +1,164 @@
+
+const projectsList = [
+    {
+        name: "The Chamber",
+
+        description: "The Chamber is a procedurally generated dungeon crawler with pathfinding enemies, over 1000 hand drawn sprites and over 100 unique items. Each level of the infinitly sprawling dungeon has a procedurally generated floor plan: each with its own random layout, connections, and aesthetic. Enemies, friendly NPCs, and chests full of useful loot can randomly spawn. Beating enemies and exploring the dungeon help level the player up, which allows them to increase their stats using the skill tree. These stat increasements can aid the player in beating the boss that resides at the end of each floor.",
+        tags: ["python", "pathfinding", "games", "favourites"],
+        image: "chamber",
+    },
+    {
+        name: "Robot Maze",
+
+        description: "I created a pathfinding controller to guide a robot through a randomly generated maze. This project was written in java and made use of my research into pathfinding algorithms such as djikstra and the A* algorithm. ",
+        tags: ["java", "pathfinding"],
+        image: "robot-maze",
+
+    },
+    {
+        name: "Large Arithmetic Collider",
+
+        description: "The Large Arithmetic Collider is a complex puzzle that involved rotating, transposing and manipulating lists in haskell. I wrote my own tree structure to consider the different decisions in order to solve the puzzle as efficiently as possible.",
+        tags: ["haskell", "functional", "favourites"],
+        image: "ldc",
+
+    },
+    {
+        name: "Ncurses Text Editor",
+        description: "I created a text editor written in C using the ncurses library in order to deliver a text-user-interface to allow the user to navigate through menus. I added the functionality to scroll, manipulate files and copy and paste within files.",
+        tags: ["c"],
+        image: "ncurses",
+    },
+    {
+        name: "How should society adapt to the autonomy of labour?",
+        description: "An essay contemplating the future of automation in the workplace and possible reactions society might take.",
+        tags: ["essay"],
+        image: "essay",
+    },
+    {
+        name: "Investigating security vulnerabilities in Linux",
+        description: "An investigation into linux security, where I identified possible security errors in a linux filesystem and webserver.",
+        tags: ["linux"],
+        image: "security-linux",
+    },
+    {
+        name: "SYTES: A task managing web app",
+        description: "SYTES is a task managing application, where users can create and log into accounts and create their own 'syte' which contains a number of tasks the user has created. Users can invite other people to their 'syte', sort their tasks, and send email notifications.",
+        tags: ["html", "css", "javascript", "SQL", "PHP", "web-dev"],
+        image: "sytes",
+    },
+    {
+        name: "Scratch Clone",
+
+        description: "Scratch is a high-level visual programming langauge that uses draggable blocks to form procedures. Using Haskell I wrote a back end to interpret blockly blocks. Users of the program can assemble a simple program by dragging blocks to perform arithmetic on values stored in the memory of the program.",
+        tags: ["haskell", "functional"],
+        image: "scratch-clone",
+
+    },
+    {
+        name: "WAFFLES: A data structure project",
+
+        description: "A project that emphasised the application of data structures and sorting algorithms content I had learnt at University. The backend for a restaurant review website I wrote utilised AVL trees, binary searches and hashmaps to efficiently store and locate data for the system.",
+        tags: ["java"],
+        image: "waffles",
+
+    },
+    {
+        name: "Personal Website 1.0",
+
+        description: "The first iteration of my personal website. I wrote this first iteraton in plain html, css and javascript.",
+        tags: ["html", "css", "javascript", "web-dev"],
+        image: "old-website",
+
+    },
+    {
+        name: "Warwick Labour Website Redesign",
+
+        description: "I created a new website for a society at my university. The previous design was created in wordpress and was in much need of a redesign. Using react and typescript I created a flat, modern, simple redesign for the society.",
+        tags: ["typescript", "react", "web-dev"],
+        image: "warwick-labour-website",
+
+    },
+    {
+        name: "Multithreaded packet sniffer",
+
+        description: "A multithreaded packet sniffer that applies my packet structure knowledge and threading methods to produce a basic intrusion detection system. The program sniffs packets and informs the user if it detects ARP cache poisoning, SYN flooding or a blacklisted URL.",
+        tags: ["c", "favourites"],
+        image: "../images/projects/robot-maze.png",
+
+    },
+    {
+        name: "SQL Gig Scheduler",
+
+        description: "In this project I was tasked to write the SQl queries required for a music production company to book acts for an upcoming festival. This involved designing a system and displaying the appropriate data required to schedule, remove and get information about gigs.",
+        tags: ["postgresql", "sql", "java"],
+        image: "../images/projects/robot-maze.png",
+
+    },
+    {
+        name: "Ursina Elements",
+
+        description: "A simple minecraft clone written in python. The player may move, place and destroy blocks, and combine items in their inventory (inspired by the main mechanic in little alchemy) to unlock new building materials. ",
+        tags: ["python"],
+        image: "ursina-elements",
+
+    },
+    {
+        name: "GO Tools",
+
+        description: "An app built for a niche board game called GO. The app allows the user to quickly convert between the different currencies, and calculate the cost of journeys between cities easily without having to consult the paper guide.",
+        tags: ["swift", "app-dev"],
+        image: "go-tools",
+
+    },
+    {
+        name: "AI Timetable Scheduler",
+
+        description: "Using artificial intelligence concepts and techniques, such as backtracking and simulated annealing, I created the methods required to create valid timetables matching comedians to demographics that they would appeal to.",
+        tags: ["python", "ai"],
+        image: "ai-timetable",
+
+    },
+    {
+        name: "Hurdle",
+
+        description: "A haskell implementation of the hit game Wordle. The player must guess a word, and each letter will be highlighted green if it is in the correct place, amber if it exists in the word but incorectly placed, or grey if not present at all.",
+        tags: ["haskell", "functional"],
+        image: "hurdle",
+
+    },
+    {
+        name: "Personal Website 2.0",
+
+        description: "The second iteration of my personal website. The layout is largely similar however since writing this version I have learnt more about React and efficient web design, so I have since moved on from this design.",
+        tags: ["typescript", "react", "web-dev"],
+        image: "website2",
+
+    },
+    {
+        name: "Mentor Connect",
+
+        description: "Mentor Connect is a web application that matches mentees to mentors based on heuristics obtained through machine learning. This project was undertaken as part of a group for Deutsche Bank, and allowed me to implement complex methods in django, to be deployed for the front end.",
+        tags: ["typescript", "react", "web-dev", "python", "django", "postgresql", "sql"],
+        image: "mentor-connect",
+
+    },
+    {
+        name: "Tautology Checker",
+
+        description: "I wrote a program to check whether logical statements are tautologies or not, using prolog. The program works by apply resolution rules and backtracking ",
+        tags: ["prolog"],
+        image: "resolution",
+
+    },
+    {
+        name: "Writing a Parser",
+
+        description: "Using javacc, I wrote a parser for a Programming Language of the Moment. The parser has protected keywords, rules for matching brackets and a grammar that the language should follow. This project allowed me to apply theory I have enjoyed learning in my degree in a practical way.",
+        tags: ["java", "parser", "javacc"],
+        image: "parser",
+
+    },
+
+]
+export default projectsList;
